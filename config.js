cb725f96c52e9be94d4ced569e3ba2ffdd6118d0
Vinay Krishna <vinaykrishna1989@gmail.com>
2021-10-17 14:46:12 -0400

    reverting to admin db user

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   rethinkdb: {
     host: process.env.RETHINKDB_HOST || '127.0.0.1',
     port: process.env.RETHINKDB_PORT || 28015,
-    username: process.env.RETHINKDB_USERNAME || 'chat',
+    username: process.env.RETHINKDB_USERNAME || 'admin',
     password: process.env.RETHINKDB_PASSWORD || '',
     db: process.env.RETHINKDB_NAME || 'chat'
   },
