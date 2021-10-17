2776fa31738eca64a49b874398eafd6562512a19
Vinay Krishna <vinaykrishna1989@gmail.com>
2021-10-17 12:56:05 -0400

    try admin user with no password

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
   rethinkdb: {
     host: process.env.RETHINKDB_HOST || '127.0.0.1',
     port: process.env.RETHINKDB_PORT || 28015,
-    username: process.env.RETHINKDB_USERNAME || 'chat',
-    password: process.env.RETHINKDB_PASSWORD || 'UNQp5wlIvD87EC7yjz0ofJ',
+    username: process.env.RETHINKDB_USERNAME || 'admin',
+    password: process.env.RETHINKDB_PASSWORD || '',
     db: process.env.RETHINKDB_NAME || 'chat'
   },
   express: {
